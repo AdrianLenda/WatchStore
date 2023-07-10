60fb0229ead3d7820c64bc2d68c6a35ef041051c
adrian <adrian.lendaaaa@gmail.com>
2023-07-10 17:18:46 +0200

    .

--- WatchStore.API/Program.cs
+++ WatchStore.API/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using WatchStore.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -9,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
